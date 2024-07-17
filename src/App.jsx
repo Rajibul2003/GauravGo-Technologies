54f3ff514c0c3e932e897e313f56f1f2806064b5
Rohan-tiwary <rohankumartiwary1406@gmail.com>
2024-07-17 23:33:31 +0530

    Deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Navbar/>
-    </>
+  <>
+  </>
   )
 }
 
