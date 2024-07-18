472209328d9ae0ed02e745313c571d99714988e7
Rohan-tiwary <rohankumartiwary1406@gmail.com>
2024-07-18 07:29:33 +0530

    Added navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import React from 'react'
+import Navbar from './component/Navbar/Navbar'
 
-function App() {
+const App = () => {
   return (
-  <>
-  
-  </>
+    <div>
+      <Navbar/>
+    </div>
   )
 }
 
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <div className='nav'>
+                <img src="src/assets/Gauravgologo.png" alt="" className="nav__logo" />
+            <a href="#" className='nav__brand'>
+                GauravGo
+            </a>
+            <ul className={`nav__menu ${isMenuOpen ? 'nav__menu--active' : ''}`}>
+                <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Our Games</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Our Studio</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Advertiser</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Contact Us</a></li>
+            </ul>
+            <div className="nav__toggler" onClick={handleToggle}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
